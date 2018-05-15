4b453954ae33b482fd514e207e663d2c3da82f08
Martín Mtz <martin.ml@icloud.com>
2018-05-15 16:24:25 -0500

    resp

--- public/app/modules/responder-encuesta/controllers/responder-encuesta.controller.js
+++ public/app/modules/responder-encuesta/controllers/responder-encuesta.controller.js
@@ -14,12 +14,13 @@
                     id_encuesta: $scope.encuestaId
                 }, function (data) {
                     $rootScope.spin = false;
-                    $scope.encuesta = data;
-                    console.log($internal.encuesta);
-                    if (data.id !== undefined) {
+                    $scope.encuesta = data.data;
+                    if (data.success) {
                         $scope.init();
                     } else {
-                        $window.location = '#/inicio-alert/' + $scope.encuestaId;
+                        // $window.location = '#/inicio-alert/' + $scope.encuestaId;
+                        $rootScope.alert = true;
+                        $rootScope.mensajeAlerta = data.msjError;
                     }
                 }, function (e) {
                     $rootScope.spin = false;
