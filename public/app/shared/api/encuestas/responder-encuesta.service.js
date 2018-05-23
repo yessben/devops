b216254f515e5223a95ba20db05c79592d0cc7d5
Martín Mtz <martin.ml@icloud.com>
2018-05-22 22:25:26 -0500

    start

--- public/app/shared/api/encuestas/responder-encuesta.service.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    angular
-        .module('api.encuestas.responder', [])
-        .service('$responderEncuesta',
-            function ($resource) {
-                var url = 'https://arcane-garden-60088.herokuapp.com/v0/responder-encuesta';
-                return $resource(url);
-            });
-})();
