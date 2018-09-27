649c7615eedfc3dc35361eaceac54056af9e5214
Martín Mtz <martin.ml@icloud.com>
2018-09-27 02:42:06 -0500

    up 45%

--- public/assets/libs/controller.js
+++ public/assets/libs/controller.js
@@ -5,13 +5,14 @@
         $(document).ready(function(){
             $("header a").on('click', function(event) {
                 if (this.hash !== "") {event.preventDefault();var hash = this.hash;$('html, body').animate({
-                  scrollTop: $(hash).offset().top - 106
+                  scrollTop: $(hash).offset().top - 80
                 }, 800, function(){
                     //window.location.hash = hash;
                 });
               }
             });
           });
+          
     
         this.startIndex = 1;
         this.slidesNumber = 3;
@@ -21,5 +22,35 @@
             this.slides.push( { image: 'url(http://localhost:4001/assets/img/'+i+'.jpg)'});
         };
       
+        $scope.categorias = [
+            {key: 'todo',         name: 'Todo', active: true},
+            {key: 'rostro',         name: 'Rostro', active: false},
+            {key: 'cuerpo',         name: 'Cuespo', active: false},
+            {key: 'piel',           name: 'Piel', active: false },
+            {key: 'procedimientos', name: 'Procedimientos', active: false }
+        ];
+
+        $scope.changeFilter = function(list, obj){
+            for(var i in list){
+                list[i].active = false;
+            }
+            obj.active = true;
+        };
+        $scope.menuShow = false;
+        $scope.showMenu = function(){
+            $scope.menuShow = true
+        };
+        $scope.hideMenu = function(){
+            $scope.menuShow = false;
+          };
+
+          $('h1').parallaxContent({
+            shift: -15,
+            duration: 1
+          });
+          $('#contentInfo').parallaxContent({
+            shift: -15,duration: 2
+          });
+
       });
 })();
